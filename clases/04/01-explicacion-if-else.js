3ef79dfc38afe495ae6049d3ddf23b7008e40491
stephan-caylent <117462968+stephan-caylent@users.noreply.github.com>
2024-04-04 21:53:41 -0500

    clase4

---
+++ clases/04/01-explicacion-if-else.js
@@ -0,0 +1,71 @@
+/*
+    - Spacov, Stephan
+    es una estrutura usada en programacion 
+    para tomar una decision con base en la respuesta 
+    de una pregunta
+
+    por ejemplo: 
+    Va a llover? (condición -> pregunta)
+    - Si la respues es si (verdad): uso paraguas
+    - Sino (falso): no lo uso
+*/
+
+/*
+    - Lujan, Jessika
+    "if else es una pregunta seguida de una afirmacion de verdadero y falso"
+    "o sea es como la pregunta para la accion pues si es verdadera o no"
+*/
+let vaLlover = 123;
+if (vaLlover === true)
+{
+    console.log("Uso paraguas");
+}
+else
+{
+    console.log("no lo uso");
+}
+
+
+
+
+
+
+
+
+
+
+let edad = 25;
+
+if (edad > 18)
+{
+    console.log("Puedes ver XVideos");
+}
+else
+{
+    console.log("Vea Paw Patrol");
+}
+
+
+
+
+
+
+
+
+
+let salud = 10;
+
+if (salud > 5) // Si!
+{
+    console.log("A");
+}
+else
+{
+    console.log("B");
+}
+
+/*
+EL COGIGO ADENTRO DEL IF SOLO SI
+EJECUTA CUANDO LA RESPUESTA DE LA
+PREGUNTA ES SI!
+*/
---
+++ clases/04/02-test.js
@@ -0,0 +1,66 @@
+//true = verdad, si
+//false = mentira, falso, no
+let salud = 8;
+let lluvia = true;
+
+if (salud >= 5 && lluvia === false)
+{
+    console.log("A");
+}
+else
+{
+    console.log("B");
+}
+
+//  pregunta 1 and pregunta 2
+//  pregunta 1: 
+// Mi salud esta buena? -> salud >= 5?
+
+//  PREGUNTA 2: 
+// No esta lluviendo? -> lluvia === false?,
+// Hace sol?
+//  RESPUESTA
+// Si 🌞
+// No 🌧️
+
+
+/*
+    Cuando un profesor pregunta: "Que entendiste?"
+    1 - Entendi nada!
+    El contrario de "entendi nada"?
+    
+    - "No entendi nada" = "Entendi todo"
+
+    2 - Hace sol -> No hace sol
+
+    3 - Cual es el contrario de hace sol ?
+        - No hace sol
+    
+    4 - Contario de negro?
+        - No negro
+    
+    5 - Contrario de Mujer?
+        - No mujer
+
+        
+    No entendi nada = Entendi todo
+
+
+    - Eso es el contrario de "Esta lloviendo"
+    No lluvia?    
+        - Si ☀️
+        - No 🌧️
+            - Esta lloviendo        
+
+
+
+
+    lluvia 🌧️
+
+    No lluvia ☀️
+
+    No esta lloviendo?
+    
+    No 🌧️? = Esta sol? = ☀️?
+
+*/
